152c9aab3e8c29c0514000c3f2e9a4b90536be5c
Samrand <samrand@engineer.com>
2024-12-19 17:56:50 +0800

    Fix third bugs with of unhandled user validation, and also other occurance

--- debug.js
+++ debug.js
@@ -5,7 +5,7 @@ const mongoose = require('mongoose');
 const app = express();
 
 
-// BUG 1
+// BUG #1
 /* This db initialization has a problem in a case of not connecting to the database
     there will be an instant crash hence we need to handle it using then and catch that are necessary
     to handle the error using catch and also handle the success using then
@@ -24,7 +24,7 @@ mongoose.connect(process.env.MONGO_URI || '' )
     )
 
 
-// BUG 2
+// BUG #2
 /*
     The user model below has lack of validations, as those must be added the basic required fields
     besides email must be ensured to be unique as we would never had the same user with the same email address
@@ -47,10 +47,52 @@ const User  = mongoose.model('User',
 
 app.use(express.json());
 
+// BUG #3
+/*
+    The user route only shows the 201 status code in the response when the user is created,
+    Meanwhile in violation of the validation there must be response code of 400
+    and also to make sure in case of any other unhandled error hence we must use try catch
+
+    OLD CODE
+    app.post('/users', async (req, res) => {
+        const user = new User(req.body);
+        await user.save();
+        res.status(201).send({ message: 'User created', user });
+    });
+
+ */
 app.post('/users', async (req, res) => {
-    const user = new User(req.body);
-    await user.save();
-    res.status(201).send({ message: 'User created', user });
+    try{
+        const [name, email] = [req.body.name, req.body.email];
+        if(!name || !email){
+            return res.status(400).send({ error: true, message: 'Name and email are required' });
+        }
+
+        const user = new User({ name, email });
+        await user.save();
+        res.status(201).send({ message: 'User created', user });
+    }catch (err) {
+        next(err);
+    }
 });
+
+// Global Error Handling Middleware
+app.use((err, req, res, next) => {
+    let statusCode = 500;
+    let message = 'Internal Server Error';
+
+    if (err.name === 'ValidationError') {
+        statusCode = 400;
+        message = Object.values(err.errors).map(val => val.message).join(', ');
+    }
+
+    if (err.code && err.code === 11000) {
+        statusCode = 400;
+        message = 'Duplicate email value entered';
+    }
+
+    res.status(statusCode).json({ error: true, message });
+});
+
 
 app.listen(3000, () => console.log('Server running on port 3000'));
