667354051343d059ee5216b4a555909f388e0fd9
Samrand <samrand@engineer.com>
2024-12-19 17:21:30 +0800

    push the main logic of the application in the app.js

---
+++ app.js
@@ -0,0 +1,25 @@
+require('dotenv').config();
+
+const express = require('express');
+const mongoose = require('mongoose');
+const userRoutes = require('src/routes/users');
+const notFound = require('src/middleware/notFound');
+const errorHandler = require('src/middleware/errorHandler');
+
+const MONGO_URI = process.env.MONGO_URI || '';
+
+mongoose.connect(MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+    .then(() => console.log('MongoDB Connected'))
+    .catch(err => console.error('MongoDB connection error:', err));
+
+const app = express();
+
+app.use('/api/users', userRoutes);
+
+app.use(notFound);
+app.use(errorHandler);
+
+module.exports = app;
